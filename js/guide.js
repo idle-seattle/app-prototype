9cf168c2be6fe3149ccb7efda8184c996e79a4ad
Dan Hiester <dan.hiester@gmail.com>
2017-02-19 17:55:59 -0800

    First push to gh-pages.

---
+++ js/guide.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var e=function(){window.getComputedStyle(document.querySelector("html"),":before").content,document.querySelector(".guide-nav1"),document.querySelector(".guide-header .btn-open")},t=function(){window.addEventListener("resize",o(),!1),document.querySelector("html").classList.add("js"),document.querySelector("html").classList.remove("no-js"),e()},n=!1,o=function(){n||(n=setTimeout(function(){n=null,e()},66))};document.addEventListener("DOMContentLoaded",t())}();
+//# sourceMappingURL=guide.js.map
